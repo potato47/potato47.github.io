618fb61d9c643f259600b6acc9b33843f0a6ad80
potato47 <1406099478@qq.com>
2023-03-31 15:43:52 +0800

    feat:测试博客

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	// Enable Vue to support Vue components.
-	integrations: [vue()],
-	site: 'https://astronaut.github.io',
+	integrations: [mdx(), sitemap(), vue()],
+	site: 'https://potato47.github.io',
 });
